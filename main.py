730cd3221ffc3ca8df0cc98949d403609b94ae33
Iris-1009 <Iris200781@gmail.com>
2025-06-13 14:26:45 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
 from microbit import *
 
+def on_forever():
+    motorSpeed = 15
+
+    #line following
+    lineSensors.display_sensors()
+    if lineSensors.check_sensor(IRSensor.MIDDLE, IRColour.BLACK):
+        motion.drive_straight(motorSpeed)
+    elif lineSensors.check_sensor(IRSensor.LEFT, IRColour.BLACK):
+        motion.turn_left(motorSpeed)
+    elif lineSensors.check_sensor(IRSensor.RIGHT, IRColour.BLACK):
+        motion.turn_right(motorSpeed)
+        
+    # pausing at obstacle 25cm away
+    if sonar.check_sonar() < 25:
+        motion.stop()
+    else:
+        motion.drive_straight(15)
+
+    #moving around obstacle 
+    distance = sonar.check_sonar()
+    if distance > 25:
+        motion.drive_straight(20)
+    else:
+        motion.drive_straight(-20)
+        basic.pause(400)
+
+        if Math.random_boolean():
+            motion.turn_left(10)
+        else:
+            motion.turn_right(10)
+        basic.pause(400)
+    
+basic.forever(on_forever)
+
+
--- main.ts
+++ main.ts
@@ -1,1 +1,36 @@
-
+basic.forever(function on_forever() {
+    let motorSpeed = 15
+    // line following
+    lineSensors.displaySensors()
+    if (lineSensors.checkSensor(IRSensor.MIDDLE, IRColour.BLACK)) {
+        motion.driveStraight(motorSpeed)
+    } else if (lineSensors.checkSensor(IRSensor.LEFT, IRColour.BLACK)) {
+        motion.turnLeft(motorSpeed)
+    } else if (lineSensors.checkSensor(IRSensor.RIGHT, IRColour.BLACK)) {
+        motion.turnRight(motorSpeed)
+    }
+    
+    //  pausing at obstacle 25cm away
+    if (sonar.checkSonar() < 25) {
+        motion.stop()
+    } else {
+        motion.driveStraight(15)
+    }
+    
+    // moving around obstacle 
+    let distance = sonar.checkSonar()
+    if (distance > 25) {
+        motion.driveStraight(20)
+    } else {
+        motion.driveStraight(-20)
+        basic.pause(400)
+        if (Math.randomBoolean()) {
+            motion.turnLeft(10)
+        } else {
+            motion.turnRight(10)
+        }
+        
+        basic.pause(400)
+    }
+    
+})
