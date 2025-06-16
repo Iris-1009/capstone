edd5ab4f6b56e6f4576eb3e9e302de1f0987f928
Iris-1009 <Iris200781@gmail.com>
2025-06-16 15:29:11 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,37 +1,95 @@
 from microbit import *
 
-def on_forever():
-    motorSpeed = 15
+motorSpeed = 15
 
-    #line following
-    lineSensors.display_sensors()
+#Line Following
+def on_forever():
+    lineSensors.display_sensors() 
+    #uses the middle sensor to detect black, proceeds if true
     if lineSensors.check_sensor(IRSensor.MIDDLE, IRColour.BLACK):
         motion.drive_straight(motorSpeed)
+    #uses the left sensor to detect black, turns left if true   
     elif lineSensors.check_sensor(IRSensor.LEFT, IRColour.BLACK):
         motion.turn_left(motorSpeed)
+    #uses the right sensor to detect black, turns right if true
     elif lineSensors.check_sensor(IRSensor.RIGHT, IRColour.BLACK):
         motion.turn_right(motorSpeed)
-        
-    # pausing at obstacle 25cm away
-    if sonar.check_sonar() < 25:
+    #if white detected, vehicle stops (no path to follow)
+    else:
         motion.stop()
-    else:
-        motion.drive_straight(15)
-
-    #moving around obstacle 
-    distance = sonar.check_sonar()
-    if distance > 25:
-        motion.drive_straight(20)
-    else:
-        motion.drive_straight(-20)
-        basic.pause(400)
-
-        if Math.random_boolean():
-            motion.turn_left(10)
-        else:
-            motion.turn_right(10)
-        basic.pause(400)
-    
 basic.forever(on_forever)
 
+# Obstacle Avoidance
+def on_forever2():
+    distance = sonar.check_sonar() #gets the distance between object and sensor
+    if distance > 25: #distance further than 25cm, proceeds 
+        motion.drive_straight(20)
+    else: #if too close
+        motion.drive_straight(-20) #reverse 
+        basic.pause(400)
+        #picks a random direction to turn
+        if Math.random_boolean() == True:  
+            motion.turn_left(10)
+        else:
+            motion.turn_right(10) 
+        basic.pause(400)
+basic.forever(on_forever2)
 
+#Stopping at Obstacle closer than 20cm
+def on_forever3():
+    if sonar.check_sonar() < 20: 
+        motion.stop()
+    else:
+        motion.drive_straight(20)
+basic.forever(on_forever3)
+
+#Driving Functions based off time
+#Forward 
+def DriveForward(seconds: number):
+    motion.drive_straight(10)
+    control.wait_micros(seconds * 1000000) #converts to microseconds
+    motion.stop()
+    
+#Reverse
+def DriveBackward(seconds: number):
+    motion.drive_straight(-10)
+    control.wait_micros(seconds * 1000000)
+    motion.stop()
+#Turns  
+def Turn(direction: str, seconds: numer):
+    if direction == "left":
+        motion.turn_left(5)
+    elif direction == "right":
+        motion.turn_right(5)
+    control.wait_micros(seconds * 1000000)
+    motion.stop()
+
+#Simple Example
+def on_button_pressed_a():
+    DriveForward(1)
+    Turn("left", 0.5)
+    DriveForward(2)
+    DriveBackward(1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+#Driving by following given instructions in N, E, S, W in a list (this could be combined with bfs algorithm)
+instructions = ["N", "E", "N", "N", "W"]
+def on_button_pressed_a1():
+    for direction in instructions:
+        if direction == "N":
+            DriveForward(1)
+        elif direction == "E":
+            Turn("right", 0.5)
+            DriveForward(0.5)
+        elif direction == "W":
+            Turn("left", 0.5)
+            DriveForward(0.5)
+        elif direction == "S":
+            DriveBackward(1)
+input.on_button_pressed(Button.A, on_button_pressed_a1)
+
+#features 
+#1. line following
+#2. obstacle avoidance 
+#4. maybe just show normal driving abilities (turn, reverse, forward)
+#5. go through map, set up with 2d grid
--- main.ts
+++ main.ts
@@ -1,30 +1,36 @@
+let motorSpeed = 15
+// Line Following
 basic.forever(function on_forever() {
-    let motorSpeed = 15
-    // line following
     lineSensors.displaySensors()
+    // uses the middle sensor to detect black, proceeds if true
     if (lineSensors.checkSensor(IRSensor.MIDDLE, IRColour.BLACK)) {
         motion.driveStraight(motorSpeed)
     } else if (lineSensors.checkSensor(IRSensor.LEFT, IRColour.BLACK)) {
+        // uses the left sensor to detect black, turns left if true   
         motion.turnLeft(motorSpeed)
     } else if (lineSensors.checkSensor(IRSensor.RIGHT, IRColour.BLACK)) {
+        // uses the right sensor to detect black, turns right if true
         motion.turnRight(motorSpeed)
-    }
-    
-    //  pausing at obstacle 25cm away
-    if (sonar.checkSonar() < 25) {
-        motion.stop()
     } else {
-        motion.driveStraight(15)
+        // if white detected, vehicle stops (no path to follow)
+        motion.stop()
     }
     
-    // moving around obstacle 
+})
+//  Obstacle Avoidance
+basic.forever(function on_forever2() {
     let distance = sonar.checkSonar()
+    // gets the distance between object and sensor
     if (distance > 25) {
+        // distance further than 25cm, proceeds 
         motion.driveStraight(20)
     } else {
+        // if too close
         motion.driveStraight(-20)
+        // reverse 
         basic.pause(400)
-        if (Math.randomBoolean()) {
+        // picks a random direction to turn
+        if (Math.randomBoolean() == true) {
             motion.turnLeft(10)
         } else {
             motion.turnRight(10)
@@ -33,4 +39,66 @@ basic.forever(function on_forever() {
         basic.pause(400)
     }
     
+})
+// Stopping at Obstacle closer than 20cm
+basic.forever(function on_forever3() {
+    if (sonar.checkSonar() < 20) {
+        motion.stop()
+    } else {
+        motion.driveStraight(20)
+    }
+    
+})
+// Driving Functions based off time
+// Forward 
+function DriveForward(seconds: number) {
+    motion.driveStraight(10)
+    control.waitMicros(seconds * 1000000)
+    // converts to microseconds
+    motion.stop()
+}
+
+// Reverse
+function DriveBackward(seconds: number) {
+    motion.driveStraight(-10)
+    control.waitMicros(seconds * 1000000)
+    motion.stop()
+}
+
+// Turns  
+function Turn(direction: string, seconds: number) {
+    if (direction == "left") {
+        motion.turnLeft(5)
+    } else if (direction == "right") {
+        motion.turnRight(5)
+    }
+    
+    control.waitMicros(seconds * 1000000)
+    motion.stop()
+}
+
+// Simple Example
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    DriveForward(1)
+    Turn("left", 0.5)
+    DriveForward(2)
+    DriveBackward(1)
+})
+// Driving by following given instructions in N, E, S, W in a list (this could be combined with bfs algorithm)
+let instructions = ["N", "E", "N", "N", "W"]
+input.onButtonPressed(Button.A, function on_button_pressed_a1() {
+    for (let direction of instructions) {
+        if (direction == "N") {
+            DriveForward(1)
+        } else if (direction == "E") {
+            Turn("right", 0.5)
+            DriveForward(0.5)
+        } else if (direction == "W") {
+            Turn("left", 0.5)
+            DriveForward(0.5)
+        } else if (direction == "S") {
+            DriveBackward(1)
+        }
+        
+    }
 })
